fbcf4e2402741bf205d064445251f6afa6bd6068
Espadasin <herostudiohd34@gmail.com>
2025-09-01 11:50:11 -0300

    Update

--- src/services/db.js
+++ src/services/db.js
@@ -2,11 +2,7 @@ import axios from "axios"
 
 
 let db = axios.create({
-<<<<<<< HEAD
     baseURL: 'https://smartcity-backend.up.railway.app'
-=======
-    baseURL: 'https://smartcity-backend.up.railway.app/'
->>>>>>> f4f4f68 (Update)
 });
 
 
